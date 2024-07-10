54b71efff2c1fe8de231904e5c491e3b94c13f2d
Ibrahim-Omran <omranibraheem705@gmail.com>
2024-07-10 18:21:03 +0300

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'core/app/env.variables.dart';
 import 'firebase_options.dart';
 import 'omran_store_app.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const OmranStoreApp());
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const OmranStoreApp());
+  });
+
+
 }
 
