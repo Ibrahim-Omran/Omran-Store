2e39121e92bdf2edac2c31b85c787add1e35609b
Ibrahim-Omran <omranibraheem705@gmail.com>
2024-07-11 01:36:16 +0300

    connectivity plus done

--- lib/omran_store_app.dart
+++ lib/omran_store_app.dart
@@ -1,24 +1,48 @@
 import 'package:flutter/material.dart';
-
+import 'package:omran_store/core/app/connectivity_controller.dart';
 import 'package:omran_store/core/app/env.variables.dart';
+import 'package:omran_store/core/common/screens/no_network_screen.dart';
 
 class OmranStoreApp extends StatelessWidget {
   const OmranStoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Omran Store App',
-      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Omran Store'),
-        ),
-      ),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, __) {
+        if (value) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            title: 'Store App',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            builder: (context, widget) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  },
+                ),
+              );
+            },
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('Store App'),
+              ),
+            ),
+          );
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            title: 'No Network',
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
   }
 }
