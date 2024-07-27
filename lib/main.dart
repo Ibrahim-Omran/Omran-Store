a510efe184a33213d06e0046e75e0df26e1dbd24
Ibrahim-Omran <omranibraheem705@gmail.com>
2024-07-28 00:51:14 +0300

    setup freezed, flutter bloc and shardPref

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:omran_store/core/app/bloc_observer.dart';
 
 import 'package:omran_store/core/app/env.variables.dart';
 import 'package:omran_store/firebase_options.dart';
@@ -15,6 +17,8 @@ void main()async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  Bloc.observer = AppBlocObserver();
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
